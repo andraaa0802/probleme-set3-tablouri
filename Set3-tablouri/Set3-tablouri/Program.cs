a8848558df79a651e20e41023fb887f03d26e312
Vinter Andrada <vinterandrada08@gmail.com>
2022-01-04 16:02:04 +0200

    amr 28 si 29

--- Set3-tablouri/Set3-tablouri/Program.cs
+++ Set3-tablouri/Set3-tablouri/Program.cs
@@ -276,24 +276,109 @@ namespace Set3_tablouri
         /// </summary>
         private static void P26()
         {
-            int n, m, i, item_nr1, item_nr2,suma,carry,produs,poz=0,j;
+            int cif1, cif2, n=0, m=0, i, item_nr1, item_nr2, suma, carry, produs, poz=0, j, sum, num, s,gasit=0,aux,lungime;
+            int[] nr1 = new int[10001];
+            int[] nr2 = new int[10001];
             Console.WriteLine("Cate cifre are primul numar?");
-            n = int.Parse(Console.ReadLine());
+            cif1 = int.Parse(Console.ReadLine());
             Console.WriteLine("Introduceti cifrele primului numar pe linii separate:");
-            int[] nr1 = new int[n];
-            for (i = 0; i < n; i++)
-                nr1[i] = int.Parse(Console.ReadLine());
+            int[] s1 = new int[cif1];
+            for (i = 0; i < cif1; i++)
+            {
+                s1[i] = int.Parse(Console.ReadLine());
+                if (s1[i] > 9 || s1[i] < 0)
+                {
+                    Console.WriteLine("Trebuie sa introduceti doar cifre naturale!");
+                    Environment.Exit(0);
+                }
+            }
             Console.WriteLine("Cate cifre are al doilea numar?");
-            m = int.Parse(Console.ReadLine());
+            cif2 = int.Parse(Console.ReadLine());
             Console.WriteLine("Introduceti cifrele celui de-al doilea numar pe linii separate:");
-            int[] nr2 = new int[m];
-            for (i = 0; i < m; i++)
-                nr2[i] = int.Parse(Console.ReadLine());
-            Array.Reverse(nr1);
-            Array.Reverse(nr2);
-            int lungime = Math.Max(n,m);
+            int[] s2 = new int[cif2];
+            for (i = 0; i < cif2; i++)
+            {
+                s2[i] = int.Parse(Console.ReadLine());
+                if (s2[i] > 9 || s2[i] < 0)
+                {
+                    Console.WriteLine("Trebuie sa introduceti doar cifre naturale!");
+                    Environment.Exit(0);
+                }
+            }
+
+            //verificare care numar este mai mare, iar apoi stocare in nr1 (numarul cel mai mare) si in nr2 (numarul cel mai mic)
+            if (cif1>cif2)
+            {
+                for (i = 0; i < cif1; i++)
+                    nr1[i] = s1[i];
+                for (j = 0; j < cif2; j++)
+                    nr2[j] = s2[j];
+                n = cif1;
+                m = cif2;
+                gasit = 1;
+            }
+            else if (cif2>cif1)
+            {
+                for (i = 0; i < cif2; i++)
+                    nr1[i] = s2[i];
+                for (j = 0; j < cif1; j++)
+                    nr2[j] = s1[j];
+                n = cif2;
+                m = cif1;
+                gasit = 2;
+            }
+            else if (cif1==cif2)
+            {
+                gasit = 0; //numerele (s1 si s2) sunt egale
+                for(i=0;i<cif1;i++)
+                {
+                    if(s1[i]>s2[i])
+                    {
+                        gasit = 1; //primul nr (s1) este mai mare
+                        break;
+                    }
+                    else if (s2[i]>s1[i])
+                    {
+                        gasit = 2; //al doilea nr (s2) este mai mare
+                    }
+                }
+                if (gasit == 0 || gasit==1)
+                {
+                    for (i = 0; i < cif1; i++)
+                    {
+                        nr1[i] = s1[i];
+                        nr2[i] = s2[i];
+                    }
+                }
+                if(gasit==2)
+                {
+                    for(i=0;i<cif1;i++)
+                    {
+                        nr1[i] = s2[i];
+                        nr2[i] = s1[i];
+                    }
+                }
+                n = m = cif1;
+            }
+            
+            //inversez ordinea elementelor din nr1 si nr2 (primul devine ultimul, al doilea devine penultimul etc.)
+            for(i=0;i<=(n-1)/2;i++)
+            {
+                aux = nr1[i];
+                nr1[i] = nr1[n - 1 - i];
+                nr1[n - 1 - i] = aux;
+            }
+            for (i = 0; i <= (m - 1) / 2; i++)
+            {
+                aux = nr2[i];
+                nr2[i] = nr2[m - 1 - i];
+                nr2[m - 1 - i] = aux;
+            }
+
+            lungime = n; //se stie ca n reprezinta numarul maxim de cifre
+
             //pentru suma:
-            int[] rezultat_s = new int[101];
+            int[] rezultat_s = new int[lungime+1];
             for(i=0;i<lungime;i++)
             {
                 item_nr1 = (i < n) ? nr1[i] : 0;
@@ -306,12 +391,12 @@ namespace Set3_tablouri
                 rezultat_s[i + 1] += carry;
             }
             Console.WriteLine("Suma este: ");
-            for(i=lungime-1;i>=0;i--)
+            for(i=lungime;i>=0;i--)
                 Console.Write(rezultat_s[i]+" ");
             Console.WriteLine();
 
             //pentru diferenta:
-            int[] rezultat_d = new int[101];
+            int[] rezultat_d = new int[lungime];
             for (i = 0; i < lungime; i++)
             {
                 item_nr1 = (i < n) ? nr1[i] : 0;
@@ -327,27 +412,34 @@ namespace Set3_tablouri
                 }
             }
             Console.WriteLine("Diferenta este: " );
+            if(gasit==2) //daca, initial al doilea numar a fost cel mai mare, atunci diferenta numerelor este negativa si scriu un "-" in fata rezultatului
+                Console.Write("- ");
             for (i = lungime-1; i >= 0; i--)
                 Console.Write(rezultat_d[i] + " ");
             Console.WriteLine();
+
             //pentru produs:
-            int[] rezultat_p = new int[101];
-            carry = 0;
+            int[] rezultat_p = new int[n+m];
+            s = 0;
             for(i=0;i<n;i++)
-            {
-                poz = 0;
+            {   
+                carry = 0;
+                poz = s;
                 for(j=0;j<m;j++)
                 {
-                    produs = rezultat_p[poz+i]+nr1[i] * nr2[j];
-                    rezultat_p[poz+i] += produs % 10;
-                    carry = produs / 10;
-                    rezultat_p[poz +i+ 1] += carry;
+                    produs = nr1[i] * nr2[j];
+                    sum = produs + rezultat_p[poz] + carry;
+                    num = sum % 10;
+                    carry = sum / 10;
+                    rezultat_p[poz] = num;
                     poz++;
                 }
+                rezultat_p[poz] = rezultat_p[poz] + carry;
+                s++;
             }
-            for(i=n*m-1;i>=0;i--)
+            Console.WriteLine("Produsul este: ");
+            for(i=n+m-1;i>=0;i--)
                 Console.Write(rezultat_p[i]+" ");
-
 
         }
         /// <summary>
